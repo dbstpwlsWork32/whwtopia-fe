345359a2909428cec91db5eee37e3f23ce0854ef
sejin-group <dbstpwlswork32@gmail.com>
2020-12-28 04:12:36 +0900

    [update] home/card empty content

--- src/views/Home/Card.vue
+++ src/views/Home/Card.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="atom_card m__card_list">
   <h2 class="_title"><slot name="title"></slot></h2>
-  <ol>
+  <ol v-if="posts.length">
     <li v-for="post in posts" :key="`post-${post.id}`" class="atom_list">
       <router-link :to="`/post/${post.id}`">
         <div class="atom_list__top">
@@ -26,6 +26,13 @@
       </router-link>
     </li>
   </ol>
+  <div style="text-align: center; padding-top: .5rem" v-else>
+    <img
+      :src="require('@assets/images/empty.png')"
+      :srcset="`${require('@assets/images/empty.png')} 1x, ${require('@assets/images/empty@2x.png')} 2x`"
+      alt="empty content"
+    />
+  </div>
 </div>
 </template>
 
