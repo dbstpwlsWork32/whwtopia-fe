140a2852a1c496e3e20799042e4c7124c667297c
sejin-group <dbstpwlswork32@gmail.com>
2020-12-30 04:33:55 +0900

    [update] divide atom component

--- src/views/Home/atoms/CardList.vue
+++ src/views/Home/atoms/CardList.vue
@@ -2,31 +2,7 @@
 <div class="atom_card">
   <h2 class="s_mb-content"><slot name="title"></slot></h2>
   <template v-if="posts.length">
-    <ol>
-      <li v-for="post in posts" :key="`post-${post.id}`" class="atom_list">
-        <router-link :to="`/post/${post.id}`">
-          <div class="atom_list__top">
-            <h3 class="s_cl-base _left">{{ post.title }}</h3>
-            <p class="s_cl-sub _right">{{ $formatDate(post.date) }}</p>
-          </div>
-          <div class="atom_list__bottom">
-            <p class="_left">{{ post.gallary }}</p>
-            <p class="s_cl-sub _right">{{ post.writer }}</p>
-          </div>
-          <div class="s_cl-sub atom_list__footer">
-            <div class="_details">
-              <p><font-awesome-icon icon="eye" aria-label="view :" /> {{ $formatNumberFlag(post.view) }}</p>
-              <p><font-awesome-icon icon="heart" aria-label="like :" /> {{ $formatNumberFlag(post.like) }}</p>
-            </div>
-            <div class="_tags">
-              <p
-                v-for="(tag, index) in post.tags" :key="`post-${post.id}-tag${index}`"
-              >#{{tag}}</p>
-            </div>
-          </div>
-        </router-link>
-      </li>
-    </ol>
+    <post-li :posts="posts" />
     <router-link to="/" class="a_more s_mt-content">더보기</router-link>
   </template>
   <div style="text-align: center; padding-top: .5rem" v-else>
@@ -41,11 +17,15 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import PostLi from '@/components/atoms/PostLi.vue'
 
 export default defineComponent({
   name: 'viewsHome_Card',
   props: {
     posts: Array
+  },
+  components: {
+    PostLi
   }
 })
 </script>
