4e614ce5520eef5d116ba1d6719a02853c32d32c
sejin-group <dbstpwlswork32@gmail.com>
2021-01-07 20:09:36 +0900

    (router) [create] view sample page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import Home from '@/views/Home/index.vue'
+import PostView from '@/views/PostView/index.vue'
+import Gallery from '@/views/Gallery/index.vue'
+import UserDetail from '@/views/UserDetail/index.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/post/:id',
+    name: 'Post',
+    component: PostView
+  },
+  {
+    path: '/gallery/:id',
+    name: 'Gallery',
+    component: Gallery
+  },
+  {
+    path: '/user/:id',
+    name: 'UserDetail',
+    component: UserDetail
   }
 ]
 
---
+++ src/views/PostView/index.vue
@@ -0,0 +1,13 @@
+<template>
+<section>
+  postivew
+</section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'view_post-view'
+})
+</script>
