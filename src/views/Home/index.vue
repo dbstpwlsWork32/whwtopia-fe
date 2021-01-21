85dc3392621c8fbc150b5a86ef9175ceef655f45
sejin-group <dbstpwlswork32@gmail.com>
2021-01-21 19:40:08 +0900

    (pub) [refactoring] repo structure

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import PostPreview from '@/components/Post/Preview/index.vue'
+import PostPreview from '@/components/PostPreview/index.vue'
 
 const samplePosts: WhwPostPreviewItem[] = [
   {
