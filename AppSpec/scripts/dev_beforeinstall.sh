4a02db663c2330da345dfb594ed8f701af8d6893
sejin-group <dbstpwlswork32@gmail.com>
2021-01-25 02:30:20 +0900

    [update] ci cd

---
+++ AppSpec/scripts/dev_beforeinstall.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+echo "invoke codedeploy fe hooks as dev"
+
+rm -rf /var/www/dev.whwtopia.kro.kr/*
---
+++ AppSpec/scripts/prod_beforeinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "invoke codedeploy fe beforeinstall as prod"
+
+rm -rf /var/www/whwtopia.kro.kr/*
